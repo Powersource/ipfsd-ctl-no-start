e2ffa2cc279ba1326655446b19189e6184178fb4
Jacob Karlsson <jacob.karlsson95@gmail.com>
2018-04-26 17:00:23 +0200

    Change to using a non-disposable node

--- index.js
+++ index.js
@@ -2,15 +2,36 @@ const f = require('ipfsd-ctl').create({type: 'go'})
 
 console.log('Running Node')
 
-f.spawn({disposable: true}, (err, ipfsd) => {
+f.spawn({repoPath: './repo/', disposable: false}, (err, ipfsd) => {
   if (err) { throw err }
 
   console.log('Spawned ipfs')
-  
-  ipfsd.api.id(function (err, id) {
-    if (err) { throw err }
-    
-    console.log(id)
-    ipfsd.stop()
+
+  //console.log('ipfsd object:', ipfsd)
+
+  ipfsd.init({directory: ipfsd.repoPath}, (err, ipfsd2) => {
+    if (err) {
+      console.error('Failed to initialize repo (might already be created):', err)
+    } else {
+      console.log('Initialized repo')
+    }
+
+    console.log('ipfsd:', ipfsd2)
+
+    ipfsd.start({}, (err, ipfs) => {
+      if (err) {
+        console.error('Failed to start:', err)
+      } else {
+        console.log('Started ipfs')
+      }
+
+      ipfs.id(function (err, id) {
+        if (err) { throw err }
+        
+        console.log('id:', id)
+        ipfsd.stop()
+      })
+
+    })
   })
 })
